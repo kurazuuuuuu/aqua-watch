3f97b311cf587e1937f7ddf9abd033ac1942d58e
kurazuuuuuu <110393398+kurazuuuuuu@users.noreply.github.com>
2025-09-05 02:30:43 +0900

    fix: admin

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -3,7 +3,7 @@ export const getBaseUrl = () => {
   if (window.location.hostname === 'aqua-watch.krz-tech.net') {
     return 'https://aqua-watch.krz-tech.net';
   }
-  return 'http://localhost:11101';
+  return 'http://localhost:11100'; // Nginxプロキシ経由でアクセス
 };
 
 export const getApiBaseUrl = () => `${getBaseUrl()}/api`;
